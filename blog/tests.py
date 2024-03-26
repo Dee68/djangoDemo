afdfcc3a2c28d6388a71a44d7ee1f43453a87c9b
dimie egberipou <ddimie283@gmail.com>
2024-03-26 15:53:40 +0000

    add test for views.

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import Post
+from django.urls import reverse
+from django.http import HttpResponse
 
 
 class ModelTesting(TestCase):
@@ -11,3 +13,8 @@ class ModelTesting(TestCase):
         d = self.blog
         self.assertTrue(isinstance(d, Post))
         self.assertEqual(str(d), 'django demo')
+
+    def test_say_something(self):
+        response = self.client.get(reverse('saySomething'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,'<h1 style="color:green">What is wrong with the leaders of today?</h1>', html=True)
