2d938029a7c1a176326b8204cd54b539cc8c7b1b
dimie egberipou <ddimie283@gmail.com>
2024-03-26 15:17:57 +0000

    Add view and urls to display.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 # from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def saySomething(request):
+    return HttpResponse('<h1 style="color:green">What is wrong with the leaders of today?</h1>')
