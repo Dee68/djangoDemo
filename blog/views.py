4675e2743cee76cee5853507bfce625ed7e3e449
dimie egberipou <ddimie283@gmail.com>
2024-03-26 15:23:37 +0000

    Correct view file for blank lines.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+
 def saySomething(request):
     return HttpResponse('<h1 style="color:green">What is wrong with the leaders of today?</h1>')
